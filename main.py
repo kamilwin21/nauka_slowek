f7cc1206db88ffd4e6645172e86c29cc384ba6b9
kamilwin21 <65176862+kamilwin21@users.noreply.github.com>
2025-05-15 20:29:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from database import SessionLocal
 
 app = FastAPI()
 
 @app.get("/test")
 def read_root():
   return {"message" : "Hello world"}
+
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
