a2b72edac134013ee2fd0332540a9f4171c9b69e
kamilwin21 <65176862+kamilwin21@users.noreply.github.com>
2025-05-17 20:48:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-from fastapi import FastAPI
-from database import SessionLocal
-import models
+# from fastapi import FastAPI
+# from database import SessionLocal
+# import models
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+# app = FastAPI()
 
-@app.get("/test")
-def read_root():
-  return {"message" : "Hello world"}
+# @app.get("/test")
+# def read_root():
+#   return {"message" : "Hello world"}
 
-def get_db():
-  db = SessionLocal()
-  try:
-    yield db
-  finally:
-    db.close()
+# def get_db():
+#   db = SessionLocal()
+#   try:
+#     yield db
+#   finally:
+#     db.close()
