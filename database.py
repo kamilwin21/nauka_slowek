6ef214617281e8483a1b714686f46f9a022846c2
kamilwin21 <65176862+kamilwin21@users.noreply.github.com>
2025-05-18 18:38:07 +0200

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./words.db"
+# DATABASE_URL = "sqlite:///./words.db"
+DATABASE_URL = "postgresql://words_9kcm_user:UdzQYKQHMaMrEAWlZM2sKmtqZN9ZMBPF@dpg-d0l0ncffte5s7393k6q0-a/words_9kcm"
+
 
 engine = create_engine(
     DATABASE_URL, connect_args={"check_same_thread": False}
